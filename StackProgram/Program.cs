5dd835c52ecae48757d78fee13e95d61a615f016
vibha <vibhauc@gmail.com>
2022-02-28 18:49:46 +0530

    [Vibha]Added:To dequeue from the begining

--- StackProgram/Program.cs
+++ StackProgram/Program.cs
@@ -14,6 +14,10 @@ namespace StackProgram
             queue.Enqueue(30);
             queue.Enqueue(70);
             queue.Display();
+            queue.Dequeue();
+
+            queue.IsEmpty();
+            queue.Display();
 
 
 
--- StackProgram/Queue.cs
+++ StackProgram/Queue.cs
@@ -29,6 +29,16 @@ namespace StackProgram
             Console.WriteLine("{0} inserted into Queue", node.data);
         }
 
+        public void Peek()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty!");
+                return;
+            }
+            Console.WriteLine("\n{0} is in the top of the Queue", this.head.data);
+        }
+
         internal void Display()
         {
             Node temp = this.head;
@@ -41,6 +51,26 @@ namespace StackProgram
             {
                 Console.WriteLine(temp.data + " ");
                 temp = temp.next;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty, Dequeue is not possible");
+                return;
+            }
+            Console.WriteLine("Dequeued value is : {0}", head.data);
+            this.head = head.next;
+        }
+
+        public void IsEmpty()
+        {
+            while (this.head != null)
+            {
+                Peek();
+                Dequeue();
             }
         }
     }
