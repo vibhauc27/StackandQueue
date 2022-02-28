9d26fa52e5c71718b155a660d70598fb14d1d8c4
vibha <vibhauc@gmail.com>
2022-02-28 17:20:20 +0530

    [Vibha]Added:To create a stack of 56-30-70

---
+++ StackProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StackProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked list stack program");
+
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StackProgram/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackProgram
+{
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
