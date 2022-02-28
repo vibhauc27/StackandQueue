bf50236b03f87e5bf91b4a745c0070b75433174f
vibha <vibhauc@gmail.com>
2022-02-28 18:19:39 +0530

    [Vibha]Added:To create a queue

--- StackProgram/Program.cs
+++ StackProgram/Program.cs
@@ -8,15 +8,16 @@ namespace StackProgram
         {
             Console.WriteLine("Linked list Stack and Queue program");
 
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-            stack.Peek();
-            stack.Pop();
-            //stack.IsEmpty();
-            //stack.Display();
+            Queue queue = new Queue();
+
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+
+
+
         }
     }
 }
---
+++ StackProgram/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProgram
+{
+    public class Queue
+    {
+        Node head = null;
+
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;                
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
